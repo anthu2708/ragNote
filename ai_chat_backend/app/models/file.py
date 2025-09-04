fff168dc1700f5ae020e9611996ceebbdd7fab1f
An Phung <phungtranthuan2005@gmail.com>
2025-09-03 18:47:01 -0700

    Resolve Enum Conflict

--- ai_chat_backend/app/models/file.py
+++ ai_chat_backend/app/models/file.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, String, DateTime, ForeignKey, LargeBinary, func
+from enum import Enum as PyEnum
+from sqlalchemy import Enum as SqlEnum
+from sqlalchemy import Column, String, DateTime, ForeignKey, LargeBinary, func, BigInteger
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from uuid import uuid4
 from datetime import datetime
 from ..database import Base
-from sqlalchemy import Enum
 
-class FileStatus(str, enum.Enum):
+class FileStatus(str, PyEnum):
     requested = "requested"
     uploaded  = "uploaded"
     ingesting = "ingesting"
@@ -29,7 +30,7 @@ class File(Base):
     key = Column(String, nullable=False)
     etag = Column(String)
     size = Column(BigInteger)
-    status = Column(Enum(FileStatus, names="file_status", validate_strings=True),
+    status = Column(SqlEnum(FileStatus, names="file_status", validate_strings=True),
                     nullable=False, server_default=FileStatus.requested.value)
 
     chat = relationship("Chat", back_populates="files")
